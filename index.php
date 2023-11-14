7c59f60d91beef8b5150ea1e35ddfe35e21a1c77
AhmedOsman101 <trollingosman101@outlook.com>
2023-11-14 11:49:00 +0200

    added dark mode

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-dark text-light">
     <div class="container">
         <form action="index.php" method="post">
             <div class="mt-5 mb-3">

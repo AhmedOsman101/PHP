bab93f2bcc5660de46f5ac1d1e036f659f8997b4
AhmadOthman <trollingosman101@outlook.com>
2023-11-20 18:25:25 +0200

    solved 2 one problem

--- problems.py
+++ problems.py
@@ -15,5 +15,27 @@ def transform_sentence(s):
             elif words[i][j - 1].lower() > words[i][j].lower():
                 words[i] = words[i][:j] + words[i][j].lower() + words[i][j + 1:]
     return " ".join(words)
-print(transform_sentence("coOL dog")) # output: "cOOl dOg"
+# print(transform_sentence("coOL dog")) # output: "cOOl dOg"
 
+def best_friend(txt, a, b):
+    text = txt.split(" ")
+    for j in text:
+        if len(j)<=1 and a in j or a in j and b not in j:
+            return False
+        else:
+            for i in range(len(j)-1):
+                if j[i] == a:
+                    if j[i+1] == b:
+                        pass
+                    else:
+                        return False
+                elif j[-1] == a:
+                    return False
+    return True
+
+def best_friends(t,a,b):
+    return t.count(a)==t.count(a+b)
+
+
+print(best_friends("he headed to the tea store", "h", "e"))
+print(best_friends('i found an ounce with my hound', 'o', 'u'))

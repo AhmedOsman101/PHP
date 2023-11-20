07a70a7d279c9990d0c666ed588ea2051ae561ae
AhmadOthman <trollingosman101@outlook.com>
2023-11-20 22:52:30 +0200

    finished odd_or_even problem

--- problems.php
+++ problems.php
@@ -415,6 +415,16 @@ function isIsogram($str)
     return true;
 }
 
+
+function odd_or_even(array $a): string
+{
+    $total = array_reduce($a, function ($prev, $current) {
+        $prev += $current;
+        return $prev;
+    });
+    return $total % 2 == 0 ? "even" : "odd";
+}
+print_r(odd_or_even([0]));
 function removeDuplicateIds($arr)
 {
 

4b9120d339730e257df9c9632222d5a3a48da4de
AhmedOsman101 <trollingosman101@outlook.com>
2023-12-13 11:20:24 +0200

    adf

--- validation/scripts/sign-up.php
+++ validation/scripts/sign-up.php
@@ -14,6 +14,7 @@ $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysql = require(__DIR__ . "/db/database.php");
 
-$sql = "INSERT INTO `customers`(`name`, `email`, `password`, `phone_number`, `Gender`, `age`, `address_1`, `address_2`, `created_at`, `updated_on`) VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? )";
+$sql = "INSERT INTO `customers`(`name`, `email`, `password`, `phone_number`, `Gender`, `age`, `address_1`, `address_2`, `created_at`, `updated_on`) 
+        VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? )";
 
 var_export($_POST);

7104e8eeb074f7136536b8675a0d0021ed2a0b0e
AhmedOsman101 <trollingosman101@outlook.com>
2023-12-13 11:19:52 +0200

    got gender value

--- validation/scripts/sign-up.php
+++ validation/scripts/sign-up.php
@@ -3,10 +3,17 @@
 if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) die("valid email is required");
 if (strlen($_POST["password"]) < 9) die("password is not valid");
 
+if (isset($_POST['radio-group'])) {
+    $selected_gender = &$_POST['radio-group'];
+    if ($selected_gender == 'male') $selected_gender = true;
+    else if ($selected_gender == 'female') $selected_gender = false;
+} else echo "Please select a gender.";
+
+
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysql = require(__DIR__ . "/db/database.php");
 
 $sql = "INSERT INTO `customers`(`name`, `email`, `password`, `phone_number`, `Gender`, `age`, `address_1`, `address_2`, `created_at`, `updated_on`) VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? )";
 
-// var_export($mysql);
+var_export($_POST);

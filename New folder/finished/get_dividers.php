6d6e025f6e0da02da26c98550394d0f47027e528
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:31:38 +0200

    extracted code into file

---
+++ New folder/finished/get_dividers.php
@@ -0,0 +1,4 @@
+function get_dividers($values, $powers) {
+$total = 1;
+$res = array();
+for ($i = 0; $i < sizeof($values); $i++) { $total *=pow($values[$i], $powers[$i]); } for ($i=1; $i <=$total; $i++) { if ($total % $i==0) { array_push($res, $i); } } return $res; }

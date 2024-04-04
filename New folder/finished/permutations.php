fd0e358a1795e43d535b70c5da535f255990e78d
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:58:16 +0200

    extracted code into file

---
+++ New folder/finished/permutations.php
@@ -0,0 +1,15 @@
+function permutations($s) {
+$res = [];
+if (count($s) == 3) {
+// array_push($res);
+}
+$sorted = false;
+while (!$sorted) {
+}
+
+
+sort($res);
+return $res;
+}
+
+// print_r(permutations(["cab", "abc", "cba"]));

ad6b4bcd0925b109e209f54b55cd1180ccb4455d
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:54:07 +0200

    extracted code into file

---
+++ New folder/finished/fibonacci_tabular_1.php
@@ -0,0 +1,4 @@
+function fibonacci_tabular_1($n) {
+$table = array_fill(0, $n + 1, 0);
+$table[1] = 1;
+for ($i = 0; $i <= $n; $i++) { @$table[$i + 1] +=$table[$i]; @$table[$i + 2] +=$table[$i]; } return $table[$n]; } // var_export(fibonacci_tabular_1(500));
---
+++ New folder/finished/fibs_fizz_buzz.php
@@ -0,0 +1,14 @@
+function fibs_fizz_buzz($n) {
+$res = [];
+foreach (range(1, $n) as $numbers) {
+if (fibonacci_memo($numbers) % 3 == 0 and fibonacci_memo($numbers) % 5 == 0) array_push($res, 'FizzBuzz');
+elseif (fibonacci_memo($numbers) % 3 == 0) array_push($res, 'Fizz');
+elseif (fibonacci_memo($numbers) % 5 == 0) array_push($res, 'Buzz');
+else {
+array_push($res, fibonacci_memo($numbers));
+print(fibonacci_memo($numbers) . "\n");
+}
+}
+return $res;
+}
+// print_r(fibs_fizz_buzz(20));

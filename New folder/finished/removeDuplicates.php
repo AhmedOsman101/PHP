a73f70cc691ba30ab977286af598cbbe45553d16
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:55:11 +0200

    extracted code into file

---
+++ New folder/finished/removeDuplicates.php
@@ -0,0 +1,7 @@
+function removeDuplicates(&$nums) {
+return array_merge(array_unique($nums), array_fill(0, sizeof($nums) - sizeof(array_unique($nums)), "_"));
+}
+
+$nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// var_export(array_reverse($nums));
+// var_export(removeDuplicates($nums) == [0, 1, 2, 3, 4, "_", "_", "_", "_", "_"]);
---
+++ New folder/finished/twoSum.php
@@ -0,0 +1,2 @@
+function twoSum($nums, $target) {
+for ($i = 0; $i < sizeof($nums); $i++) { if (in_array($target - $nums[$i], $nums) and $target - $nums[$i] !=$nums[$i]) return [$i, array_search($target - $nums[$i], $nums)]; } }

51808945010d6ad24c38b5c465758b8e0f2cc523
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:44:54 +0200

    extracted code into file

---
+++ New folder/finished/narcissistic.php
@@ -0,0 +1,10 @@
+function narcissistic(int $value): bool {
+$value = str_split("$value");
+$size = sizeof($value);
+$sum = 0;
+foreach ($value as $num) {
+$sum += pow($num, $size);
+}
+return $sum == (implode($value));
+}
+// var_export(narcissistic(153));
---
+++ New folder/finished/tribonacci.php
@@ -0,0 +1,7 @@
+function tribonacci($signature, $n) {
+$res = [];
+if ($n == 0) return $res;
+$i = 0;
+while (true) {
+if (sizeof($res) == $n) break;
+if ($i <= 2) { array_push($res, $signature[$i]); } else if ($i==3) { array_push($res, array_sum($res)); } else { array_push($res, array_sum([$res[$i - 3], $res[$i - 2], $res[$i - 1]])); } $i++; } return $res; } // print_r(tribonacci([1, 1, 1], 1000));

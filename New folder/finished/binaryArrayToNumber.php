0f1c21b4dabe0f99138f59aa6ba753782e1d188e
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:24:45 +0200

    extracted code into file

---
+++ New folder/finished/binaryArrayToNumber.php
@@ -0,0 +1,5 @@
+function binaryArrayToNumber($arr) {
+$arr = array_reverse($arr);
+$res = 0;
+$multiplier = 1;
+for ($i = 0; $i < sizeof($arr); $i++) { $res +=$arr[$i] * $multiplier; $multiplier *=2; } return $res; }

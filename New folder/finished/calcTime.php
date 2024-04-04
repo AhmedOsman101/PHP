e0c15d77bcee8a72ceba11afcf1fdca077efc7c1
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:50:52 +0200

    extracted code into file

--- New folder/finished/calcTime.php
+++ New folder/finished/calcTime.php
@@ -1,3 +1,20 @@
+function calc_time() {
+require('./isPrime.php');
+// Start the timer
+$start_time = microtime(true);
+
+// Call your function
+$result = (isPrime(69));
+
+// Stop the timer
+$end_time = microtime(true);
+
+// Calculate the difference in microseconds
+$execution_time = $end_time - $start_time;
+
+echo "The function took " . $execution_time . " seconds to execute.";
+}
+
 // Start the timer
 $start_time = microtime(true);
 
@@ -11,3 +28,7 @@ $end_time = microtime(true);
 $execution_time = $end_time - $start_time;
 
 echo "The function took " . $execution_time . " seconds to execute.";
+
+
+// sprintf('%.0', $execution_time)
+// calc_time();

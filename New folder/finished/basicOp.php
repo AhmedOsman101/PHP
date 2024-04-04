10207a86c3fc48e71e16fa250423bbf4cb66e490
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:38:10 +0200

    extracted code into file

---
+++ New folder/finished/basicOp.php
@@ -0,0 +1,14 @@
+function basicOp($op, $val1, $val2) {
+switch ($op) {
+case '+':
+return $val1 + $val2;
+case '-':
+return $val1 - $val2;
+case '*':
+return $val1 * $val2;
+case '/':
+return $val1 / $val2;
+default:
+return "error";
+}
+}

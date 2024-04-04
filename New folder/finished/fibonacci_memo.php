966710614792ebaba0d0717be878e383692128e9
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:52:23 +0200

    extracted code into file

---
+++ New folder/finished/fibonacci_memo.php
@@ -0,0 +1,3 @@
+function fibonacci_memo($n, &$memo = []) {
+if (in_array($n, array_keys($memo))) return $memo[$n];
+if ($n <= 2) return 1; $memo[$n]=fibonacci_memo($n - 1, $memo) + fibonacci_memo($n - 2, $memo); return $memo[$n]; } print_r(fibonacci_memo(10));

f73730010f8c5f23495c9a2bbb545a01508257de
AhmadOthman <trollingosman101@outlook.com>
2024-04-04 22:22:04 +0200

    extracted code into file

---
+++ New folder/finished/spinWords.php
@@ -0,0 +1,9 @@
+function spinWords(string $str): string {
+$str = explode(" ", $str);
+foreach ($str as &$word) {
+if (strlen($word) >= 5) {
+$word = strrev($word);
+}
+}
+return implode(" ", $str);
+}

8dfdf201e741f0c672354b652df336a367aa30c5
AhmedOsman101 <trollingosman101@outlook.com>
2024-12-09 01:09:25 +0200

    Updated Files 2024-12-09@01:09AM

--- problem solving/get_dividers.php
+++ problem solving/get_dividers.php
@@ -1,7 +1,7 @@
 <?php
 function get_dividers($values, $powers) {
     $total = 1;
-    $res = array();
+    $res = [];
     for ($i = 0; $i < sizeof($values); $i++) {
         $total *= pow($values[$i], $powers[$i]);
     }
--- problem solving/sum_strings.php
+++ problem solving/sum_strings.php
@@ -3,24 +3,24 @@ function sum_strings($a, $b) {
     [$a, $b] = [strrev($a), strrev($b)];
     $res = "";
     $len = max(strlen($a), strlen($b)) + 1;
-    $remainder = 0;
+    $carry = 0;
     for ($i = 0; $i < $len; $i++) {
         echo "S///////////////////////////////////////////////////////////////////\n";
-        $na = isset($a[$i]) ? (int)$a[$i] : 0;
-        $nb = isset($b[$i]) ? (int)$b[$i] : 0;
-        echo "values:" . $na + $nb . " remainder:" . $remainder . "\n";
-        if (($na + $nb + $remainder) < 10 and !empty($remainder)) {
-            $res .= ($na + $nb + $remainder);
-            $remainder = 0;
-        } elseif (($na + $nb + $remainder) >= 10 and !empty($remainder)) {
-            $res .= ($na + $nb + $remainder - 10);
-            $remainder = 1;
-        } elseif (($na + $nb) < 10 and empty($remainder)) {
-            $res .= $na + $nb + $remainder;
-            $remainder = 0;
-        } elseif (($na + $nb) >= 10 and empty($remainder)) {
+        $na = isset($a[$i]) ? (int) $a[$i] : 0;
+        $nb = isset($b[$i]) ? (int) $b[$i] : 0;
+        echo "values:" . $na + $nb . " remainder:" . $carry . PHP_EOL;
+        if (($na + $nb + $carry) < 10 and !empty($carry)) {
+            $res .= ($na + $nb + $carry);
+            $carry = 0;
+        } elseif (($na + $nb + $carry) >= 10 and !empty($carry)) {
+            $res .= ($na + $nb + $carry - 10);
+            $carry = 1;
+        } elseif (($na + $nb) < 10 and empty($carry)) {
+            $res .= $na + $nb + $carry;
+            $carry = 0;
+        } elseif (($na + $nb) >= 10 and empty($carry)) {
             $res .= ($na + $nb) - 10;
-            $remainder = 1;
+            $carry = 1;
         }
         echo "E///////////////////////////////////////////////////////////////////\n\n";
     }
@@ -29,5 +29,7 @@ function sum_strings($a, $b) {
 
 $a = "712569312664357328695151392";
 $b = "8100824045303269669937";
-echo sum_strings($a, $b) . "\n";
-echo ($a + $b) . "\n";
+$c = sum_strings($a, $b);
+echo "$c\n";
+echo ($a + $b) . PHP_EOL;
+var_export(($a + $b) == $c);

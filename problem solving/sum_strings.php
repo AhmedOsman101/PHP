7a812e65c4db1b6b5b7c0ea81339e6e177b9ecb8
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-09 01:51:07 +0200

    Updated Files 2025-01-09@01:51AM

--- problem solving/sum_strings.php
+++ problem solving/sum_strings.php
@@ -1,35 +1,38 @@
 <?php
-function sum_strings($a, $b) {
-    [$a, $b] = [strrev($a), strrev($b)];
-    $res = "";
-    $len = max(strlen($a), strlen($b)) + 1;
-    $carry = 0;
-    for ($i = 0; $i < $len; $i++) {
-        echo "S///////////////////////////////////////////////////////////////////\n";
-        $na = isset($a[$i]) ? (int) $a[$i] : 0;
-        $nb = isset($b[$i]) ? (int) $b[$i] : 0;
-        echo "values:" . $na + $nb . " remainder:" . $carry . PHP_EOL;
-        if (($na + $nb + $carry) < 10 and !empty($carry)) {
-            $res .= ($na + $nb + $carry);
-            $carry = 0;
-        } elseif (($na + $nb + $carry) >= 10 and !empty($carry)) {
-            $res .= ($na + $nb + $carry - 10);
-            $carry = 1;
-        } elseif (($na + $nb) < 10 and empty($carry)) {
-            $res .= $na + $nb + $carry;
-            $carry = 0;
-        } elseif (($na + $nb) >= 10 and empty($carry)) {
-            $res .= ($na + $nb) - 10;
-            $carry = 1;
-        }
-        echo "E///////////////////////////////////////////////////////////////////\n\n";
+
+declare(strict_types=1);
+
+function sum_strings($n, $m): string {
+  [$n, $m] = [strrev($n), strrev($m)];
+  $res = "";
+  $len = max(strlen($n), strlen($m)) + 1;
+  $carry = 0;
+  for ($i = 0; $i < $len; $i++) {
+    $na = isset($n[$i]) ? (int) $n[$i] : 0;
+    $nb = isset($m[$i]) ? (int) $m[$i] : 0;
+    $sum = $na + $nb + $carry;
+    echo "sum:" . $sum . " carry:" . $carry . PHP_EOL;
+    if ($sum == 0) {
+      $res .= "0";
+      continue;
+    } elseif ($sum < 10) {
+      $res .= $sum;
+      $carry = 0;
+    } else {
+      $res .= $sum - 10;
+      $carry = 1;
     }
-    return ltrim(strrev($res), "0");
+  }
+  $res = ltrim(strrev($res), "0");
+  $res = empty($res) ? "0" : $res;
+
+  return $res;
 }
 
 $a = "712569312664357328695151392";
 $b = "8100824045303269669937";
 $c = sum_strings($a, $b);
-echo "$c\n";
-echo ($a + $b) . PHP_EOL;
-var_export(($a + $b) == $c);
+
+var_dump($c);
+var_dump($a + $b);
+var_dump(($a + $b) == $c);

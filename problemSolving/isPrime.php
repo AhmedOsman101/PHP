839b839deebc196544c30ab11078cb5e41cfda4b
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-21 00:22:03 +0200

    Updated Files 2025-04-21@12:22AM

--- problemSolving/isPrime.php
+++ problemSolving/isPrime.php
@@ -1,6 +1,5 @@
 <?php
-function isPrime($n)
-{
+function isPrime($n) {
     if ($n <= 1) return false;
     if ($n == 2) return true;
     if ($n % 2 == 0) return false;
--- problemSolving/primeNum.php
+++ problemSolving/primeNum.php
@@ -1,5 +1,7 @@
 <?php
 function primeNum($num) {
-    for ($i = 2; $i < $num; $i++) if ($num % $i == 0) return false;
+    for ($i = 2; $i < $num; $i++) {
+        if ($num % $i == 0) return false;
+    }
     return true;
 }
